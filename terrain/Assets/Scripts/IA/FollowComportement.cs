07d514ff001a41dd01a131fb6c5d6d0330356df8
Raphaël <raphael.struk@gmail.com>
2016-01-26 02:24:20 +0100

    Boulvert IA

--- terrain/Assets/Scripts/IA/FollowComportement.cs
+++ terrain/Assets/Scripts/IA/FollowComportement.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Fungus;
 
 public class FollowComportement : Comportement {
 
 	public override void CalculateDestination(Transform playerDestination){
+		Flowchart flow = GameObject.FindGameObjectWithTag ("Player").GetComponent<capsule>().flow;
+		if ( flow.GetIntegerVariable ("Boulvertes") > 0) {
+
 		Transform player = GameObject.FindGameObjectWithTag ("Player").transform;
 		Vector3 dist = player.position - iapos;
 		if (dist.magnitude > 5f)
@@ -11,7 +15,10 @@ public class FollowComportement : Comportement {
 		else
 			destination = iapos;
 	//	Debug.Log (destination);
-	}
+		}
+		else
+			destination = new Vector3 (0, 0, 0);
+		
 }
 
 
